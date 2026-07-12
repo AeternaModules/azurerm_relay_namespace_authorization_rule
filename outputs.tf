0fc040f3f19c990d06008586e170e5c9fca16242
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:19:56 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,50 +1,45 @@
-output "relay_namespace_authorization_rules" {
-  description = "All relay_namespace_authorization_rule resources"
-  value       = azurerm_relay_namespace_authorization_rule.relay_namespace_authorization_rules
-  sensitive   = true
-}
 output "relay_namespace_authorization_rules_listen" {
-  description = "List of listen values across all relay_namespace_authorization_rules"
-  value       = [for k, v in azurerm_relay_namespace_authorization_rule.relay_namespace_authorization_rules : v.listen]
+  description = "Map of listen values across all relay_namespace_authorization_rules, keyed the same as var.relay_namespace_authorization_rules"
+  value       = { for k, v in azurerm_relay_namespace_authorization_rule.relay_namespace_authorization_rules : k => v.listen }
 }
 output "relay_namespace_authorization_rules_manage" {
-  description = "List of manage values across all relay_namespace_authorization_rules"
-  value       = [for k, v in azurerm_relay_namespace_authorization_rule.relay_namespace_authorization_rules : v.manage]
+  description = "Map of manage values across all relay_namespace_authorization_rules, keyed the same as var.relay_namespace_authorization_rules"
+  value       = { for k, v in azurerm_relay_namespace_authorization_rule.relay_namespace_authorization_rules : k => v.manage }
 }
 output "relay_namespace_authorization_rules_name" {
-  description = "List of name values across all relay_namespace_authorization_rules"
-  value       = [for k, v in azurerm_relay_namespace_authorization_rule.relay_namespace_authorization_rules : v.name]
+  description = "Map of name values across all relay_namespace_authorization_rules, keyed the same as var.relay_namespace_authorization_rules"
+  value       = { for k, v in azurerm_relay_namespace_authorization_rule.relay_namespace_authorization_rules : k => v.name }
 }
 output "relay_namespace_authorization_rules_namespace_name" {
-  description = "List of namespace_name values across all relay_namespace_authorization_rules"
-  value       = [for k, v in azurerm_relay_namespace_authorization_rule.relay_namespace_authorization_rules : v.namespace_name]
+  description = "Map of namespace_name values across all relay_namespace_authorization_rules, keyed the same as var.relay_namespace_authorization_rules"
+  value       = { for k, v in azurerm_relay_namespace_authorization_rule.relay_namespace_authorization_rules : k => v.namespace_name }
 }
 output "relay_namespace_authorization_rules_primary_connection_string" {
-  description = "List of primary_connection_string values across all relay_namespace_authorization_rules"
-  value       = [for k, v in azurerm_relay_namespace_authorization_rule.relay_namespace_authorization_rules : v.primary_connection_string]
+  description = "Map of primary_connection_string values across all relay_namespace_authorization_rules, keyed the same as var.relay_namespace_authorization_rules"
+  value       = { for k, v in azurerm_relay_namespace_authorization_rule.relay_namespace_authorization_rules : k => v.primary_connection_string }
   sensitive   = true
 }
 output "relay_namespace_authorization_rules_primary_key" {
-  description = "List of primary_key values across all relay_namespace_authorization_rules"
-  value       = [for k, v in azurerm_relay_namespace_authorization_rule.relay_namespace_authorization_rules : v.primary_key]
+  description = "Map of primary_key values across all relay_namespace_authorization_rules, keyed the same as var.relay_namespace_authorization_rules"
+  value       = { for k, v in azurerm_relay_namespace_authorization_rule.relay_namespace_authorization_rules : k => v.primary_key }
   sensitive   = true
 }
 output "relay_namespace_authorization_rules_resource_group_name" {
-  description = "List of resource_group_name values across all relay_namespace_authorization_rules"
-  value       = [for k, v in azurerm_relay_namespace_authorization_rule.relay_namespace_authorization_rules : v.resource_group_name]
+  description = "Map of resource_group_name values across all relay_namespace_authorization_rules, keyed the same as var.relay_namespace_authorization_rules"
+  value       = { for k, v in azurerm_relay_namespace_authorization_rule.relay_namespace_authorization_rules : k => v.resource_group_name }
 }
 output "relay_namespace_authorization_rules_secondary_connection_string" {
-  description = "List of secondary_connection_string values across all relay_namespace_authorization_rules"
-  value       = [for k, v in azurerm_relay_namespace_authorization_rule.relay_namespace_authorization_rules : v.secondary_connection_string]
+  description = "Map of secondary_connection_string values across all relay_namespace_authorization_rules, keyed the same as var.relay_namespace_authorization_rules"
+  value       = { for k, v in azurerm_relay_namespace_authorization_rule.relay_namespace_authorization_rules : k => v.secondary_connection_string }
   sensitive   = true
 }
 output "relay_namespace_authorization_rules_secondary_key" {
-  description = "List of secondary_key values across all relay_namespace_authorization_rules"
-  value       = [for k, v in azurerm_relay_namespace_authorization_rule.relay_namespace_authorization_rules : v.secondary_key]
+  description = "Map of secondary_key values across all relay_namespace_authorization_rules, keyed the same as var.relay_namespace_authorization_rules"
+  value       = { for k, v in azurerm_relay_namespace_authorization_rule.relay_namespace_authorization_rules : k => v.secondary_key }
   sensitive   = true
 }
 output "relay_namespace_authorization_rules_send" {
-  description = "List of send values across all relay_namespace_authorization_rules"
-  value       = [for k, v in azurerm_relay_namespace_authorization_rule.relay_namespace_authorization_rules : v.send]
+  description = "Map of send values across all relay_namespace_authorization_rules, keyed the same as var.relay_namespace_authorization_rules"
+  value       = { for k, v in azurerm_relay_namespace_authorization_rule.relay_namespace_authorization_rules : k => v.send }
 }
 
